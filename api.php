b6e903728974ed1e93bda4b1226e9f9073be30dd
cmrudi <cutmeurahrudi@gmail.com>
2017-09-18 22:11:16 +0700

    update test_plan and change dfile name to be intuitively recognized

--- api.php
+++ api.php
@@ -1,6 +1,6 @@
 <?php
-	if ($_GET['param'] == 'test_1' )
-		readfile('test_1.html');
-	else if ($_GET['param'] == 'test_2' )
-		readfile('test_2.html');
+	if ($_GET['size'] == '500b' )
+		readfile('content/file_500b.html');
+	else if ($_GET['size'] == '20kb' )
+		readfile('content/file_20kb.html');
 ?>
